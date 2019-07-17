0ac3fba06f119fcd35e19278e878b6f90f459b36
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2019-07-17 16:41:42 -0300

    adicionando build na pasta /docs para executar no io do github.

---
+++ docs/precache-manifest.6b025d21d017f1e008108d2c58df2a8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5a9adfb996d4b768f1db8c72b7d639",
+    "url": "./index.html"
+  },
+  {
+    "revision": "825802d7aede47bbf2f7",
+    "url": "./static/css/main.cc52c571.chunk.css"
+  },
+  {
+    "revision": "7e4cf50a63719c7f9299",
+    "url": "./static/js/2.f16f9b63.chunk.js"
+  },
+  {
+    "revision": "825802d7aede47bbf2f7",
+    "url": "./static/js/main.8a093d05.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
