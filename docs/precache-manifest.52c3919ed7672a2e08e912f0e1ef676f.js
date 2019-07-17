899564a5f8763c953184e8916c52112722ca5eb2
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2019-07-17 11:32:30 -0300

    deploy de primeira versão do build

---
+++ docs/precache-manifest.52c3919ed7672a2e08e912f0e1ef676f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077b5a8dc7da5d94cd9e6e639c2c7d7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f6641519a96d0f90e03",
+    "url": "./static/css/main.cc52c571.chunk.css"
+  },
+  {
+    "revision": "7e4cf50a63719c7f9299",
+    "url": "./static/js/2.f16f9b63.chunk.js"
+  },
+  {
+    "revision": "1f6641519a96d0f90e03",
+    "url": "./static/js/main.519f6dc9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
