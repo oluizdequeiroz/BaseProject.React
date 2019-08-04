e4972cb7bcf769ed8af906b57decc076f53bed16
LuizDeQueiroz <luizdequeiroz@outlook.com.br>
2019-08-04 20:54:28 -0300

    criando endpoint de salvar receita.

--- api/routes/receitas.js
+++ api/routes/receitas.js
@@ -16,4 +16,51 @@ router.get('/', (req, res, next) => {
     }))
 });
 
+router.post('/salvar', (req, res, next) => {
+  if (req.body.codigo) {
+    models.receita.update({ ...req.body, codigo: undefined }, {
+      where: {
+        codigo: req.body.codigo
+      }
+    })
+      .then(() => res.json({
+        sucesso: true
+      }))
+      .then(error => res.json({
+        stack: error,
+        erros: [
+          "Erro ao tentar salvar receita"
+        ]
+      }));
+  } else {
+    models.receita.create(req.body)
+      .then(() => res.json({
+        sucesso: true
+      }))
+      .then(error => res.json({
+        stack: error,
+        erros: [
+          "Erro ao tentar salvar receita"
+        ]
+      }));
+  }
+});
+
+router.delete('/excluir/:codigo', (req, res, next) => {
+  models.receita.destroy({
+    where: {
+      codigo: req.params.codigo
+    }
+  })
+    .then(() => res.json({
+      sucesso: true
+    }))
+    .then(error => res.json({
+      stack: error,
+      erros: [
+        "Erro ao tentar excluir receita"
+      ]
+    }));
+});
+
 module.exports = router;
