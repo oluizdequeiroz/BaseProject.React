9e016def3d1685de53a50a3b407677d6a1718beb
LuizDeQueiroz <luizdequeiroz@outlook.com.br>
2019-08-12 17:07:18 -0300

    ajustando consulta de refeições por cliente e de data entre inicial e final.

--- api/routes/clientes.js
+++ api/routes/clientes.js
@@ -23,20 +23,43 @@ router.get('/refeicoes/:codigoCliente/:dataInicial/:dataFinal', (req, res, next)
       as: 'dias'
     }],
     where: {
-      numsequencial: req.params.codigoCliente,
-      datarefeicao: {
-        '$Between': [req.params.dataInicial, req.params.dataFinal]
-      }
+      [models.Sequelize.Op.and]: [
+        {
+          numsequencial: req.params.codigoCliente,
+        },
+        models.Sequelize.literal(`dias.refdatarefeicao between '${req.params.dataInicial}' and '${req.params.dataFinal}'`)
+      ]
     }
   })
-    .then(clientes =>
+    .then(clientes => {
+      const cliente = clientes.length ? {
+        ...clientes[0].dataValues,
+        dias: clientes[0].dias.groupBy('datarefeicao', 'refeicoes')
+      } : { dias: [] };
+
+      let diasVazios = [];
+      const dataInicial = new Date(req.params.dataInicial);
+      const dataFinal = new Date(req.params.dataFinal);
+      let qtdDias = dataFinal.subtract(dataInicial);
+      qtdDias = qtdDias < 7 ? 7 : qtdDias;
+      const numDiasVazios = qtdDias - cliente.dias.length;
+      for (var i = 1; i <= numDiasVazios; i++) {
+        const date = numDiasVazios === qtdDias ? dataInicial : new Date(cliente.dias[cliente.dias.length - 1].datarefeicao);
+        date.setDate(dataInicial.getDate() + i + 1);
+
+        diasVazios.push({
+          datarefeicao: `${date.toISOString().substring(0, 10)}`
+        });
+      }
+
+      if (cliente.dias) cliente.dias.push(...diasVazios);
+      else cliente.dias = diasVazios;
+
       res.json({
         sucesso: true,
-        retorno: {
-          ...clientes[0].dataValues,
-          dias: clientes[0].dias.groupBy('datarefeicao', 'refeicoes')
-        }
-      }))
+        retorno: cliente
+      });
+    })
     .then(error => res.json({
       stack: error,
       erros: [
